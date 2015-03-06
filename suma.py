1ade07cc8a444a619910bbcfc3db2189d7e49c79
Yennifer Santiago <yennifersantiago@gmail.com>
2015-03-06 15:39:18 -0430

    Se agrega programa que suma dos numero solicitados por pantalla

---
+++ suma.py
@@ -0,0 +1,4 @@
+### Introducir dos nuumero por teclado y mostrar su suma en pantalla 
+h = input('Introduzca un numero: ')
+i = input('Introduzca otro numero: ')
+print 'La suma de los numeros es %s'%(h+i)
